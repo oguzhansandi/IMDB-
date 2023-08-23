8fd14fb5669fe30d9e173f5faab7121fbe24ecc4
oguzhansandi <oguzhansandi@gmail.com>
2023-08-23 19:38:04 +0300

    Create header section

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,7 @@
+import Header from './components/Header'
 import './globals.css'
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
 
-const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'IMDB Clone',
@@ -16,7 +15,15 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+      <body>
+        {/*Header*/}
+        <Header/>
+        {/*Navbar*/}
+        {/*SearchBox*/}
+      
+
+        {children}
+        </body>
+    </html> 
   )
 }
