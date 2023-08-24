4d00a8b89dc8d2636cf49e71011fa76b58d4f3fc
oguzhansandi <oguzhansandi@gmail.com>
2023-08-24 17:00:32 +0300

    created the about page

---
+++ app/about/page.tsx
@@ -0,0 +1,47 @@
+import React from 'react'
+
+export default function About() {
+  return (
+    <div className='max-w-6xl mx-auto space-y-4'> 
+        <h1 className='text-2xl font-medium text-amber-600'> About</h1>
+        <p>
+        Welcome!! Lorem ipsum dolor sit amet consectetur
+        adipisicing elit. 
+        Laudantium modi eaque repellat aliquid labore numquam 
+        corrupti consequatur! Beatae ducimus molestiae
+        eligendi dolor reiciendis odit ipsa,
+        sed voluptas! Possimus, iste veniam.
+        Lorem ipsum dolor sit amet consectetur
+        adipisicing elit. 
+        </p>
+        <p>
+        Laudantium modi eaque repellat aliquid labore numquam 
+        corrupti consequatur! Beatae ducimus molestiae
+        eligendi dolor reiciendis odit ipsa,
+        sed voluptas! Possimus, iste veniam.
+        Lorem ipsum dolor sit amet consectetur
+        adipisicing elit. 
+        </p>
+        <p>
+        Laudantium modi eaque repellat aliquid labore numquam 
+        corrupti consequatur! Beatae ducimus molestiae
+        eligendi dolor reiciendis odit ipsa,
+        sed voluptas! Possimus, iste veniam.
+        Lorem ipsum dolor sit amet consectetur
+        adipisicing elit. 
+        Laudantium modi eaque repellat aliquid labore numquam 
+        corrupti consequatur! Beatae ducimus molestiae
+        eligendi dolor reiciendis odit ipsa,
+        sed voluptas! Possimus, iste veniam.
+        Lorem ipsum dolor sit amet consectetur
+        adipisicing elit. 
+        Laudantium modi eaque repellat aliquid labore numquam 
+        corrupti consequatur! Beatae ducimus molestiae
+        eligendi dolor reiciendis odit ipsa,
+        sed voluptas! Possimus, iste veniam.
+        
+        </p>
+    </div>
+  )
+    
+}
