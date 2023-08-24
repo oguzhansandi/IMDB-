143378704206f9d5f74de71aa068b20b54b3e64b
oguzhansandi <oguzhansandi@gmail.com>
2023-08-24 22:45:57 +0300

    fetch data from TMDB. API

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,27 @@
+import { log } from 'console';
 import Image from 'next/image'
+import Results from './components/Results';
 
-export default function Home() {
+const API_KEY = process.env.API_KEY
+
+export default async function Home({ searchParams}) {
+  const genre = searchParams.genre || "fetchTrending";
+  console.log(API_KEY)
+  const res = await fetch(
+    `https://api.themoviedb.org/3/${genre === 'fetchTopRated' ? "movie/top_rated" : "movie/popular"}?api_key=${API_KEY}&language=en-US&page=1`, {next : {revalidate : 10000}} );
+
+    if(!res.ok){
+      throw new Error("Failed to fetch data");
+    }
+
+    const data = await res.json();
+    const results = data.results
+    console.log(results)
+    
   return (
-    <h1 className='text-red-400'>HOME</h1>
+
+    <div> <Results results={results}></Results> </div>
+
+
   )
 }
