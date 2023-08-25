63dbda0379958c9b2e68993a9b94c369befcf216
oguzhansandi <oguzhansandi@gmail.com>
2023-08-25 20:55:57 +0300

    added movie detail page

--- app/components/Results.tsx
+++ app/components/Results.tsx
@@ -5,7 +5,7 @@ export default function Results({results}) {
   return (
     <div className='sm:grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4
     2xl:grid-cols-5 max-w-6xl mx-auto py-4'>
-        {results.map((result) =>(
+        {results.map((result: { id: React.Key | null | undefined }) =>(
             <Card key={result.id} result={result}></Card>
         ))}
     </div>
