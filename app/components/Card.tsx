52f92e40ca98b8aa07f77e8ad5fa00ea5c4eab03
oguzhansandi <oguzhansandi@gmail.com>
2023-08-26 11:18:14 +0300

    edit poster_path

--- app/components/Card.tsx
+++ app/components/Card.tsx
@@ -9,14 +9,14 @@ export default function Card({result}) {
     sm: shadow-md rounded-lg sm:border 
     sm: border-slate-400 sm:m-2 transition-shadow duration-200 group'>
         <Link href={`/movie/${result.id}`}>
-            <Image src ={`https://image.tmdb.org/t/p/original/${result.backdrop_path || poster_path}`}
+            <Image src ={`https://image.tmdb.org/t/p/original/${result.backdrop_path || result.poster_path}`}
             width={500}
             height={300}
             className='sm:rounded-t-lg group-hover:opacity-80 transition-opacity duration-200'
             style={
                 {
                     maxWidth:"%100",
-                    height : "auto",
+                    maxHeight :"150px",
                 }
             }
             placeholder = "blur"
